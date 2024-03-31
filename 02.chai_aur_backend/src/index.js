25b80565af6347d08ae2353d0a6447985c94aa7d
Nitin Awari <nitinawari86@gmail.com>
2024-03-31 09:08:34 +0530

    connect to mongo atlas

---
+++ 02.chai_aur_backend/src/index.js
@@ -0,0 +1,51 @@
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+   path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//first way to connect dB
+/*
+import mongoose  from "mongoose"
+import { DB_NAME } from "./constants";
+import express from "express"
+const app = express()
+(async ()=>{
+   try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
+
+      app.on("error" ,(error)=>{
+         console.log("error" , error)
+         throw error
+      }) 
+      app.listen(process.env.PORT , ()=>{
+         console.log(`App is listening on port ${process.env.PORT}`)
+      })
+
+   } catch (error) {
+      console.error("Error :" , error)
+      throw error
+   }
+})
+()
+*/
